53cd10d4eb609dd8ab177ca6928e8b23ffc7361e
Tyler Barnes <tylerdbarnes@gmail.com>
2020-04-12 00:12:26 -0700

    initial commit

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,12 +3,73 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
-// You can delete this file if you're not using it
 
-/**
- * You can uncomment the following line to verify that
- * your plugin is being loaded in your site.
- *
- * See: https://www.gatsbyjs.org/docs/creating-a-local-plugin/#developing-a-local-plugin-that-is-outside-your-project
- */
-exports.onPreInit = () => console.log("Loaded gatsby-starter-plugin")
+const notifier = require(`node-notifier`)
+const path = require(`path`)
+const player = require(`play-sound`)((opts = {}))
+const open = require(`open`)
+
+const title = path.basename(process.cwd())
+const icon = path.join(__dirname, "assets/Gatsby_Monogram.png")
+const closeLabel = `close`
+
+const playSound = (soundName) => {
+  if (soundName) {
+    const sound = path.join(__dirname, `assets/sounds/${soundName}.wav`)
+    player.play(sound)
+  }
+}
+
+const defaultPluginOptions = {
+  sound: `Glass`,
+  toast: true,
+}
+
+const getPluginOptions = (pluginOptions) => ({
+  ...defaultPluginOptions,
+  ...pluginOptions,
+})
+
+exports.onCreateDevServer = (_, pluginOptions) => {
+  const options = getPluginOptions(pluginOptions)
+
+  if (options.toast) {
+    notifier.notify(
+      {
+        message: `gatsby app is ready`,
+        open: `http://localhost:8000`,
+        actions: [`open app`, `open graphiql`, `open both`],
+        closeLabel,
+        title,
+        icon,
+        timeout: 15,
+      },
+      function (_err, _response, { activationValue }) {
+        if ([`open app`, `open both`].includes(activationValue)) {
+          open(`http://localhost:8000`)
+        }
+
+        if ([`open graphiql`, `open both`].includes(activationValue)) {
+          open(`http://localhost:8000/___graphiql`)
+        }
+      }
+    )
+  }
+
+  playSound(options.sound)
+}
+
+exports.onPostBuild = (_, pluginOptions) => {
+  const options = getPluginOptions(pluginOptions)
+
+  if (options.toast) {
+    notifier.notify({
+      message: `your build has completed`,
+      closeLabel,
+      title,
+      icon,
+    })
+  }
+
+  playSound(options.sound)
+}
